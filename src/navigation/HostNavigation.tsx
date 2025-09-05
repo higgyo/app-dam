c4ce05f203c2e35cf6c9918878d735d16c45c3f0
higgyo <darinformax@gmail.com>
2025-09-05 17:18:54 +0000

    Feature: criar navegacao principal com drawer

--- src/navigation/HostNavigation.tsx
+++ src/navigation/HostNavigation.tsx
@@ -2,23 +2,33 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { LoginScreen } from '../screens/login/LoginScreen';
-
-const Stack = createNativeStackNavigator()
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 export function HostNavigation() {
     const isLogged = true
 
-    return <NavigationContainer>
-        <Stack.Navigator>
+    return (
+        <NavigationContainer>
             {isLogged ? <ProtectedRoutes /> : <PublicRoutes />}
-        </Stack.Navigator>
-    </NavigationContainer>
+        </NavigationContainer>
+    )
 }
 
+const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
 function PublicRoutes() {
-    return <><Stack.Screen name='Login' component={LoginScreen} /></>
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Login' component={LoginScreen} />
+        </Stack.Navigator>
+    )
 }
 
 function ProtectedRoutes() {
-    return <><Stack.Screen name='Home' component={HomeScreen} /></>
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name='Home' component={HomeScreen} />
+        </Drawer.Navigator>
+    )
 }
