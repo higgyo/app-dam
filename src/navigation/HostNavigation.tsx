9af7972e2c0630323a963df929cd3b4f988bb3e4
higgyo <darinformax@gmail.com>
2025-09-12 17:42:26 +0000

    WIP: construindo telas de chat

--- src/navigation/HostNavigation.tsx
+++ src/navigation/HostNavigation.tsx
@@ -5,6 +5,7 @@ import { LoginScreen } from "../screens/login/LoginScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { useAuthContext } from "../contexts/AuthContext";
 import { ChatListScreen } from "../screens/chat_list/ChatListScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 export function HostNavigation() {
     const auth = useAuthContext();
@@ -18,20 +19,49 @@ export function HostNavigation() {
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 function PublicRoutes() {
     return (
-        <Stack.Navigator initialRouteName="Chats">
-            <Stack.Screen name="Chats" component={ChatListScreen} />
+        <Stack.Navigator>
             <Stack.Screen name="Login" component={LoginScreen} />
         </Stack.Navigator>
     );
 }
 
+// Opção 1: Bottom Tab dentro do Drawer (Recomendado)
 function ProtectedRoutes() {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator initialRouteName="ChatTabs">
             <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen
+                name="ChatTabs"
+                component={ChatBottomTabs}
+                options={{ title: "Conversas" }}
+            />
         </Drawer.Navigator>
+    );
+}
+
+function ChatBottomTabs() {
+    return (
+        <BottomTab.Navigator>
+            <BottomTab.Screen
+                name="Chats"
+                component={ChatListScreen}
+                options={{
+                    tabBarLabel: "Chats",
+                    // Adicione ícones aqui se necessário
+                }}
+            />
+            <BottomTab.Screen
+                name="Groups"
+                component={ChatListScreen}
+                options={{
+                    tabBarLabel: "Grupos",
+                    // Adicione ícones aqui se necessário
+                }}
+            />
+        </BottomTab.Navigator>
     );
 }
