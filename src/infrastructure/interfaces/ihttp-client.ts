c2089cc87e8926edf1ffc66d17567c537dd1e3ef
higgyo <darinformax@gmail.com>
2025-09-26 14:44:12 -0300

    Feature: implementar repositorios

---
+++ src/infrastructure/interfaces/ihttp-client.ts
@@ -0,0 +1,12 @@
+export type Options = {
+    headers?: Record<string, string>,
+    params?: Record<string, any>
+}
+
+export interface IHttpClient {
+    get<T = unknown>(url: string, options?: Options): Promise<T>;
+    post<T = unknown>(url: string, data: any, options?: Options): Promise<T>;
+    put<T = unknown>(url: string, data: any, options?: Options): Promise<T>;
+    patch<T = unknown>(url: string, data: any, options?: Options): Promise<T>;
+    delete<T = unknown>(url: string, options?: Options): Promise<T>;
+}
