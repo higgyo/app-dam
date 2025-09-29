09ea656a33da296715dda22710aba9bde296ce9f
higgyo <darinformax@gmail.com>
2025-09-29 14:04:50 -0300

    WIP: criar factory

--- src/domain/entities/Message.ts
+++ src/domain/entities/Message.ts
@@ -3,15 +3,17 @@ export default class Message {
         readonly message: string,
         readonly date: string,
         readonly userId: string,
-        readonly id?: string,
+        readonly chatId: string,
+        readonly id?: string
     ) {}
 
-    static create(message: string, date: string, userId: string, id?: string) {
-        return new Message(
-            message,
-            date,
-            userId,
-            id
-        )
+    static create(
+        message: string,
+        date: string,
+        userId: string,
+        chatId: string,
+        id?: string
+    ) {
+        return new Message(message, date, userId, chatId, id);
     }
 }
---
+++ src/infrastructure/factories/ChatServiceFactory.ts
@@ -0,0 +1,3 @@
+export class ChatServiceFactory {
+    static create() {}
+}
