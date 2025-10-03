29f1ac83626aacba0425772ed54208f8a5b5eff3
nicolasgabrieldev <nicolascefet@hotmail.com>
2025-10-03 14:33:01 -0300

    feat: inicio use-cases

--- src/domain/value-objects/GeoLocation.ts
+++ src/domain/value-objects/GeoLocation.ts
@@ -1,5 +1,5 @@
 export default class GeoLocation {
-    private constructor(latitude: number, longitude: number) {}
+    private constructor(readonly latitude: number, readonly longitude: number) {}
 
     static create(latitude: number, longitude: number) {
         return new GeoLocation(latitude, longitude);
