556272cc9cc980c3dece03330f5d933f14ebce0a
higgyo <darinformax@gmail.com>
2025-11-09 12:05:59 -0300

    Chore: adicionar tipagem do supabase

--- src/infrastructure/supabase/index.ts
+++ src/infrastructure/supabase/index.ts
@@ -1,6 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
 
 const supabaseUrl = "https://usaqywugirnsworksgyx.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+    auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
