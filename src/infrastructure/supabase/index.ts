34867c6c9dfaa274d753c6c686eacef42519c226
higgyo <darinformax@gmail.com>
2025-11-10 14:03:45 -0300

    Chore: configurar o storage do supabase

--- src/infrastructure/supabase/index.ts
+++ src/infrastructure/supabase/index.ts
@@ -1,11 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
+import * as SecureStore from 'expo-secure-store';
 
 const supabaseUrl = "https://usaqywugirnsworksgyx.supabase.co";
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
     auth: {
+        storage: {
+            getItem: SecureStore.getItemAsync,
+            setItem: SecureStore.setItemAsync,
+            removeItem: SecureStore.deleteItemAsync,
+        },
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
