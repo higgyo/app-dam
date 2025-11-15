3e2a5b1b81a5492deeda3e2a2930ece9c2239fe3
higgyo <darinformax@gmail.com>
2025-11-14 21:07:02 -0300

    ass last

--- src/infrastructure/supabase/index.ts
+++ src/infrastructure/supabase/index.ts
@@ -1,6 +1,7 @@
+import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
 const supabaseUrl = "https://usaqywugirnsworksgyx.supabase.co";
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
